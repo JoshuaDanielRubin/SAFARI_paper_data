c99c1ea7db5f0be1be35816babc4edf893da9ebc
JoshuaDanielRubin <jdrubin@middlebury.edu>
2024-03-29 21:09:10 +0100

    Update2

---
+++ linear_experiment/human_mito/stat_results/TestSafari/plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data for plotting, based on the table provided
+data = {
+    "Deamination factor": [0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00],
+    "vg giraffe Hominin": [101200, 88719, 77684, 68250, 60127, 53311, 47019, 41896],
+    "SAFARI Hominin": [120660, 107364, 96179, 84455, 75319, 65623, 57320, 49846],
+    "vg giraffe rCRS": [66992, 57406, 49214, 42445, 36638, 32076, 27681, 24244],
+    "SAFARI rCRS": [104326, 92001, 80955, 70610, 58435, 48029, 39885, 33515]
+}
+
+# Convert data into a DataFrame
+df = pd.DataFrame(data)
+
+# Plotting
+plt.figure(figsize=(10, 6))
+
+# Plotting each line
+plt.plot(df["Deamination factor"], df["vg giraffe Hominin"], color='orange', label='vg giraffe Hominin')
+plt.plot(df["Deamination factor"], df["SAFARI Hominin"], color='green', label='SAFARI Hominin')
+plt.plot(df["Deamination factor"], df["vg giraffe rCRS"], color='orange', linestyle='--', label='vg giraffe rCRS')
+plt.plot(df["Deamination factor"], df["SAFARI rCRS"], color='green', linestyle='--', label='SAFARI rCRS')
+
+# Adding titles and labels
+plt.title('Aligned Reads Mapping: vg giraffe vs. SAFARI')
+plt.xlabel('Deamination factor')
+plt.ylabel('Number of Aligned Reads')
+plt.legend()
+
+# Show the plot
+plt.tight_layout()
+plt.savefig("hominin_results.png")
+
---
+++ linear_experiment/human_mito/stat_results/TestSafari/run.sh
@@ -0,0 +1,38 @@
+
+safari_path="/home/projects/MAAG/Magpie/Magpie/vgan_corrected/dep/vg/bin/vg"
+giraffe_path="/home/projects/MAAG/Magpie/Magpie/vgan_uncorrected/dep/vg/bin/vg"
+
+
+for RATE in 0.25 0.50 0.75 1.00 1.25 1.50 1.75 2.00; do
+
+$safari_path giraffe -t 30 -f /home/projects/MAAG/Magpie/Magpie/linear_experiment/human_mito/stat_results/TestSafari/Denisova8_100g_100_deamsim_${RATE}.fq.gz \
+           -m /home/projects2/hominin/vgan_dev/share/tmfiles/graph.min \
+           -d /home/projects2/hominin/vgan_dev/share/tmfiles/graph.dist \
+           -q /home/projects2/hominin/vgan_dev/share/tmfiles/graph.ry \
+           -Z /home/projects2/hominin/vgan_dev/share/tmfiles/graph.giraffe.gbz \
+           --deam-3p ATP2_mt_${RATE}.3p.prof --deam-5p ATP2_mt_${RATE}.5p.prof > test_safari_hominin_${RATE}.gam
+
+$giraffe_path giraffe -t 30 -f /home/projects/MAAG/Magpie/Magpie/linear_experiment/human_mito/stat_results/TestSafari/Denisova8_100g_100_deamsim_${RATE}.fq.gz \
+           -m /home/projects2/hominin/vgan_dev/share/tmfiles/graph.min \
+           -d /home/projects2/hominin/vgan_dev/share/tmfiles/graph.dist \
+           -Z /home/projects2/hominin/vgan_dev/share/tmfiles/graph.giraffe.gbz \
+           > test_giraffe_hominin_${RATE}.gam
+
+done
+
+for RATE in 0.25 0.50 0.75 1.00 1.25 1.50 1.75 2.00; do
+
+$safari_path giraffe -t 30 -f /home/projects/MAAG/Magpie/Magpie/linear_experiment/human_mito/stat_results/TestSafari/Denisova8_100g_100_deamsim_${RATE}.fq.gz \
+           -m ../../rCRS_graph/graph.min \
+           -d ../../rCRS_graph/graph.dist \
+           -q ../../rCRS_graph/graph.ry \
+           -Z ../../rCRS_graph/graph.giraffe.gbz \
+           --deam-3p ATP2_mt_${RATE}.3p.prof --deam-5p ATP2_mt_${RATE}.5p.prof > test_safari_rCRS_${RATE}.gam
+
+$giraffe_path giraffe -t 30 -f /home/projects/MAAG/Magpie/Magpie/linear_experiment/human_mito/stat_results/TestSafari/Denisova8_100g_100_deamsim_${RATE}.fq.gz \
+           -m ../../rCRS_graph/graph.min \
+           -d ../../rCRS_graph/graph.dist \
+           -Z ../../rCRS_graph/graph.giraffe.gbz \
+           > test_giraffe_rCRS_${RATE}.gam
+
+done
