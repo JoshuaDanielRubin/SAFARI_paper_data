b925e33d67531a1af64111403fc2790e16cb7ba6
JoshuaDanielRubin <jdrubin@middlebury.edu>
2024-03-31 15:45:15 +0200

    Update

--- linear_experiment/human_mito/stat_results/plot_F1_optimized_pangenome.py
+++ linear_experiment/human_mito/stat_results/plot_F1_optimized_pangenome.py
@@ -46,7 +46,7 @@ def main():
     plt.title('Median F1 Score at High Damage by Pangenome Tool \n (Optimized Parameters, ' + sys.argv[3] + ")")
     plt.xlabel('Damage Level')
     plt.ylabel('Median F1 Score')
-    plt.ylim(0.995, 0.999)
+    plt.ylim(0.998, 0.999)
     plt.legend(title='Tool')
     plt.tight_layout()
 
