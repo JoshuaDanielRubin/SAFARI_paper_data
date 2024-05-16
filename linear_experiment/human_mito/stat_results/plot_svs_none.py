fdf346704a5dbb0405421b4c639870a09c8a7e67
JoshuaDanielRubin <jdrubin@middlebury.edu>
2024-05-16 14:59:13 +0200

    Increase DPI

--- linear_experiment/human_mito/stat_results/plot_svs_none.py
+++ linear_experiment/human_mito/stat_results/plot_svs_none.py
@@ -54,7 +54,7 @@ def plot_best_sensitivity(data_path):
         else:
             plt.scatter(tool_data['sensitivity'], tool_data['specificity'], label=f'{tool}', color=tool_colors[tool], alpha=0.3, marker='^',s=80)
 
-    plt.title('sensitivity vs Specificity for Optimized Parameters \n (' + sys.argv[3] + ", No Damage)", fontsize=16, fontweight='bold')
+    plt.title('Sensitivity vs Specificity for Optimized Parameters \n (' + sys.argv[3] + ", No Damage)", fontsize=16, fontweight='bold')
     plt.xlabel('sensitivity', fontsize=14, fontweight='bold')
     plt.ylabel('Specificity', fontsize=14, fontweight='bold')
     plt.legend()
