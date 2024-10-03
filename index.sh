4c8494c6eae0e211e3f88a998f8ff2d6b8268c06
JoshuaDanielRubin <jdrubin@middlebury.edu>
2024-10-03 21:45:30 +0200

    Update

--- index.sh
+++ index.sh
@@ -1,18 +1,9 @@
-newvg="/home/projects/MAAG/Magpie/Magpie/vg_corrected/bin/vg"
+newvg="/home/projects/MAAG/Magpie/Magpie/vgan_corrected/dep/vg/bin/vg"
 
 #### HAPLOCART
 
-#$newvg minimizer -o vgan_corrected/share/vgan/hcfiles/graph.min -t 60 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
-#                -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 25 -w 12
+$newvg minimizer -o vgan_corrected/share/vgan/hcfiles/graph.min -t 50 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
+                -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 20 -w 15
 
-#$newvg rymer -o vgan_corrected/share/vgan/hcfiles/graph.ry -t 60 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
-#                -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 25 -w 12
-
-
-## LINEAR
-
-$newvg minimizer -o vgan_corrected/share/vgan/hcfiles/graph.min -t 60 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
-                -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 30 -w 15
-
-$newvg rymer -o vgan_corrected/share/vgan/hcfiles/graph.ry -t 60 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
-                -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 30 -w 15
+$newvg rymer -o vgan_corrected/share/vgan/hcfiles/graph.ry -t 50 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
+               -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 20 -w 15
--- run_euka.sh
+++ run_euka.sh
@@ -1,22 +1,15 @@
 
-#./vg_corrected/bin/vg minimizer -t 60 -p -g ../euka_dir/euka_db.gbwt -d ../euka_dir/euka_db.dist \
-#-o ../euka_dir/euka_db.min ../euka_dir/euka_db.og -k 30 -w 10 && echo -e "\n\n"
-#./vg_corrected/bin/vg rymer -t 60 -p -g ../euka_dir/euka_db.gbwt -d ../euka_dir/euka_db.dist \
-#-o ../euka_dir/euka_db.ry ../euka_dir/euka_db.og -k 30 -w 10 && echo -e "\n\n"
+#./vgan_corrected/dep/vg/bin/vg minimizer -t 40 -p -g ../euka_dir/euka_db.gbwt -d ../euka_dir/euka_db.dist \
+#-o ../euka_dir/euka_db.min ../euka_dir/euka_db.og -k 30 -w 20 && echo -e "\n\n"
+#./vgan_corrected/dep/vg/bin/vg rymer -t 40 -p -g ../euka_dir/euka_db.gbwt -d ../euka_dir/euka_db.dist \
+#-o ../euka_dir/euka_db.ry ../euka_dir/euka_db.og -k 30 -w 20 && echo -e "\n\n"
 
-#for POSTERIOR in 0.9;
-#do
-#    zcat /home/projects2/euka_environments/full_cave_dhigh.fq.gz | /usr/bin/time -v ./vgan_corrected/bin/vgan euka \
-#    -t 20 -fq1 /dev/stdin --euka_dir /home/projects/MAAG/Magpie/euka_dir/ --minFrag 100 \
-#    --deam3p /home/projects/MAAG/Magpie/Magpie/dhigh3p.prof --deam5p /home/projects/MAAG/Magpie/Magpie/dhigh5p.prof -o euka_corrected_full_j_${POSTERIOR} -j ${POSTERIOR}
-#done
+parallel -j 1 'zcat /home/projects2/euka_environments/full_cave_dhigh.fq.gz | /usr/bin/time -v ./vgan_corrected/bin/vgan euka -t 1 \
+               -fq1 /dev/stdin --euka_dir /home/projects/MAAG/Magpie/euka_dir/ --minFrag 100 --minBins 6 \
+               --deam3p /home/projects/MAAG/Magpie/Magpie/dhigh3p.prof --deam5p /home/projects/MAAG/Magpie/Magpie/dhigh5p.prof \
+              -o euka_corrected_full_j_{} -j {}' ::: $(seq 0.9 0.1 0.9)
 
-zcat /home/projects2/euka_environments/full_cave_dhigh.fq.gz | ./vgan_uncorrected/bin/vgan euka -t 20 --minFrag 100 \
--fq1 /dev/stdin --euka_dir /home/projects/MAAG/Magpie/euka_dir/ -o euka_uncorrected_full
+#zcat /home/projects2/euka_environments/full_cave_dhigh.fq.gz | ./vgan_uncorrected/bin/vgan euka -t 40 --minFrag 100 \
+#-fq1 /dev/stdin --euka_dir /home/projects/MAAG/Magpie/euka_dir/ -o euka_uncorrected_full --minBins 6 --minMQ 10 --entropy 0.5
 
 
-### TESTING
-#for POSTERIOR in 0.0;
-#do
-#./vgan_corrected/bin/vgan euka -t -1 -fq1 seq.fq --euka_dir /home/projects/MAAG/Magpie/euka_dir/ --deam3p /home/projects/MAAG/Magpie/Magpie/dhigh3p.prof --deam5p /home/projects/MAAG/Magpie/Magpie/dhigh5p.prof -o euka_corrected_full_j_${POSTERIOR} -j ${POSTERIOR}
-#done
