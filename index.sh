3eb1a728b84ab81d9e6638421fd70cbda286327d
JoshuaDanielRubin <jdrubin@middlebury.edu>
2024-10-05 18:17:58 +0200

    Update

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@ newvg="/home/projects/MAAG/Magpie/Magpie/vgan_corrected/dep/vg/bin/vg"
 #### HAPLOCART
 
 $newvg minimizer -o vgan_corrected/share/vgan/hcfiles/graph.min -t 50 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
-                -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 20 -w 15
+                -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 10 -w 2
 
 $newvg rymer -o vgan_corrected/share/vgan/hcfiles/graph.ry -t 50 -d vgan_corrected/share/vgan/hcfiles/graph.dist \
-               -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 20 -w 15
+               -g vgan_corrected/share/vgan/hcfiles/graph.gbwt -p vgan_corrected/share/vgan/hcfiles/graph.og -k 10 -w 2
